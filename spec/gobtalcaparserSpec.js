d55c64fb9b9250fa99c607a3502c1111a2b76595
Fabián Villar <fabhian_araragi_kaoru_42@live.cl>
2017-01-30 01:35:08 -0300

    Fixed indent and LF errors, all test passed.

--- spec/gobtalcaparserSpec.js
+++ spec/gobtalcaparserSpec.js
@@ -5,29 +5,29 @@ const parser = require('../src/gobtalca.parser.js')
 
 describe('GOBTALCA Parser', () => {
 
-    describe('border conditions', () => {
-        it('declares correct url', () => {
-            parser.url.should.equal('http://www.gobernaciontalca.gov.cl/noticias/')
-        })
-
-        it('declares correct name', () => {
-            parser.name.should.equal('GOBTALCA')
-        })
-
-        it('no news returns empty structure', () => {
-            const $ = cheerio.load('')
-            const parsed = parser.parser($)
-
-            parsed.should.deepEqual({
-                follow: [],
-                parsed: []
-            })
-        })
+  describe('border conditions', () => {
+    it('declares correct url', () => {
+      parser.url.should.equal('http://www.gobernaciontalca.gov.cl/noticias/')
     })
 
-    describe('news list parsing', () => {
+    it('declares correct name', () => {
+      parser.name.should.equal('GOBTALCA')
+    })
 
-        const htmlBlock = `
+    it('no news returns empty structure', () => {
+      const $ = cheerio.load('')
+      const parsed = parser.parser($)
+
+      parsed.should.deepEqual({
+        follow: [],
+        parsed: []
+      })
+    })
+  })
+
+  describe('news list parsing', () => {
+
+    const htmlBlock = `
       <div class="post tarjeta">
         <div class="pic">
           <a href="/sin-categoria/detalle-de-beneficios-para-afectados-por-incendios-forestales/">
@@ -58,67 +58,56 @@ describe('GOBTALCA Parser', () => {
       </div>
     `
 
-        it('parses correct amount of articles', () => {
-            const $ = cheerio.load(htmlBlock)
-            const parsed = parser.parser($)
+    it('parses correct amount of articles', () => {
+      const $ = cheerio.load(htmlBlock)
+      const parsed = parser.parser($)
 
-            parsed.parsed.should.not.be.null()
-            parsed.parsed.length.should.equal(2)
-        })
-
-
-        it('parses timestamp', () => {
-            const $ = cheerio.load(htmlBlock)
-            const parsed = parser.parser($)
-
-            parsed.parsed.should.not.be.null()
-            parsed.parsed[0].timestamp.should.equal('2017-01-27 00:00:00')
-            parsed.parsed[1].timestamp.should.equal('2017-01-26 00:00:00')
-        })
-
-        it('parses title', () => {
-            const $ = cheerio.load(htmlBlock)
-            const parsed = parser.parser($)
-
-            parsed.parsed.should.not.be.null()
-            parsed.parsed[0].title.should.equal('Detalle de beneficios para afectados por incendios forestales')
-            parsed.parsed[1].title.should.equal('Beneficios para afectados por incendios forestales')
-        })
-
-
-        it('parses thumbnail', () => {
-            const $ = cheerio.load(htmlBlock)
-            const parsed = parser.parser($)
-
-            parsed.parsed.should.not.be.null()
-                // eslint-disable-next-line max-len
-            parsed.parsed[0].thumbnail.should.equal('http://www.gobernaciontalca.gov.cl/media/2017/01/presi-2-320x210.jpg')
-                // eslint-disable-next-line max-len
-            parsed.parsed[1].thumbnail.should.equal('http://www.gobernaciontalca.gov.cl/media/2017/01/Logo-web-320x210.jpg')
-        })
-
-        it('parses cannonical link', () => {
-            const $ = cheerio.load(htmlBlock)
-            const parsed = parser.parser($)
-
-            parsed.parsed.should.not.be.null()
-                // eslint-disable-next-line max-len
-            parsed.parsed[0].cannonical.should.equal('http://www.gobernaciontalca.gov.cl/sin-categoria/detalle-de-beneficios-para-afectados-por-incendios-forestales/')
-                // eslint-disable-next-line max-len
-            parsed.parsed[1].cannonical.should.equal('http://www.gobernaciontalca.gov.cl/noticias/beneficios-para-afectados-por-incendios-forestales/')
-        })
-
-        it('parses follow links', () => {
-            const $ = cheerio.load(htmlBlock)
-            const parsed = parser.parser($)
-
-            parsed.parsed.should.not.be.null()
-                // eslint-disable-next-line max-len
-            parsed.follow[0].url.should.equal('http://www.ministeriodesarrollosocial.gob.cl/noticias/2017/01/28/mds-encabeza-operativo-social-en-localidad-de-los-aromos-en-el-maule')
-                // eslint-disable-next-line max-len
-            parsed.follow[1].url.should.equal('http://www.ministeriodesarrollosocial.gob.cl/noticias/2017/01/27/subsecretario-de-servicios-sociales-visito-zona-afectada-de-marchigüe')
-        })
-
+      parsed.parsed.should.not.be.null()
+      parsed.parsed.length.should.equal(2)
     })
+
+
+    it('parses timestamp', () => {
+      const $ = cheerio.load(htmlBlock)
+      const parsed = parser.parser($)
+
+      parsed.parsed.should.not.be.null()
+      parsed.parsed[0].timestamp.should.equal('2017-01-27 00:00:00')
+      parsed.parsed[1].timestamp.should.equal('2017-01-26 00:00:00')
+    })
+
+    it('parses title', () => {
+      const $ = cheerio.load(htmlBlock)
+      const parsed = parser.parser($)
+
+      parsed.parsed.should.not.be.null()
+      parsed.parsed[0].title.should.equal('Detalle de beneficios para afectados por incendios forestales')
+      parsed.parsed[1].title.should.equal('Beneficios para afectados por incendios forestales')
+    })
+
+
+    it('parses thumbnail', () => {
+      const $ = cheerio.load(htmlBlock)
+      const parsed = parser.parser($)
+
+      parsed.parsed.should.not.be.null()
+        // eslint-disable-next-line max-len
+      parsed.parsed[0].thumbnail.should.equal('http://www.gobernaciontalca.gov.cl/media/2017/01/presi-2-320x210.jpg')
+        // eslint-disable-next-line max-len
+      parsed.parsed[1].thumbnail.should.equal('http://www.gobernaciontalca.gov.cl/media/2017/01/Logo-web-320x210.jpg')
+    })
+
+    it('parses cannonical link', () => {
+      const $ = cheerio.load(htmlBlock)
+      const parsed = parser.parser($)
+
+      parsed.parsed.should.not.be.null()
+        // eslint-disable-next-line max-len
+      parsed.parsed[0].cannonical.should.equal('http://www.gobernaciontalca.gov.cl/sin-categoria/detalle-de-beneficios-para-afectados-por-incendios-forestales/')
+        // eslint-disable-next-line max-len
+      parsed.parsed[1].cannonical.should.equal('http://www.gobernaciontalca.gov.cl/noticias/beneficios-para-afectados-por-incendios-forestales/')
+    })
+
+  })
 
 })
