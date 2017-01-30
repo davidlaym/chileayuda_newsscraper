3e6da3850f1c5e56f6192b2907dc46a05f4df6b1
Fabián Villar <fabhian_araragi_kaoru_42@live.cl>
2017-01-30 01:35:08 -0300

    Fixed indent ann LF errors, all test passed.

--- src/gobtalca.parser.js
+++ src/gobtalca.parser.js
@@ -1,53 +1,54 @@
 const _ = require('lodash')
 
 exports = module.exports = {
-    name: 'GOBTALCA',
-    url: 'http://www.gobernaciontalca.gov.cl/noticias/',
-    parser: _parser
+  name: 'GOBTALCA',
+  url: 'http://www.gobernaciontalca.gov.cl/noticias/',
+  parser: _parser
 }
 
 const months = {
-    'enero': '01',
-    'febrero': '02',
-    'marzo': '03',
-    'abril': '04',
-    'mayo': '05',
-    'junio': '06',
-    'julio': '07',
-    'agosto': '08',
-    'septiembre': '09',
-    'octubre': '10',
-    'noviembre': '11',
-    'diciembre': '12'
+  'enero': '01',
+  'febrero': '02',
+  'marzo': '03',
+  'abril': '04',
+  'mayo': '05',
+  'junio': '06',
+  'julio': '07',
+  'agosto': '08',
+  'septiembre': '09',
+  'octubre': '10',
+  'noviembre': '11',
+  'diciembre': '12'
 }
 
 function _parser($) {
-    const $news = $('div.post')
-    const root = 'http://www.gobernaciontalca.gov.cl'
+  const $news = $('div.post')
+  const root = 'http://www.gobernaciontalca.gov.cl'
 
-    const parsed = _.map($news, n => ({
-        timestamp: DateToTimeStamp($(n).find('div.texto span.meta').text().trim()),
-        title: $(n).find('div.texto h4.title a').text().trim(),
-        thumbnail: root + $(n).find('img').attr('src'),
-        cannonical: root + $(n).find('h4.title a').attr('href')
-    }))
+  const parsed = _.map($news, n => ({
+    timestamp: DateToTimeStamp($(n).find('div.texto span.meta').text().trim()),
+    title: $(n).find('div.texto h4.title a').text().trim(),
+    thumbnail: root + $(n).find('img').attr('src'),
+    cannonical: root + $(n).find('h4.title a').attr('href')
+  }))
 
-    const follow = []
+  const follow = []
 
-    function DateToTimeStamp(date) {
-        const parts = date.split(' ')
-        const d = parseInt(parts[0]) > 9 ? parts[0] : '0' + parts[0]
-        const y = parts[4]
-        const m = monthNameToNumber(parts[2])
-        return `${y}-${m}-${d} 00:00:00`
-    }
+  function DateToTimeStamp(date) {
+    const parts = date.split(' ')
+    const d = parseInt(parts[0]) > 9 ? parts[0] : '0' + parts[0]
+    const y = parts[4]
+    const m = monthNameToNumber(parts[2])
+    return `${y}-${m}-${d} 00:00:00`
+  }
 
-    function monthNameToNumber(monthName) {
+  function monthNameToNumber(monthName) {
 
-        return months[monthName]
-    }
-    return {
-        parsed,
-        follow,
-    }
+    return months[monthName]
+  }
+
+  return {
+    parsed,
+    follow,
+  }
 }
